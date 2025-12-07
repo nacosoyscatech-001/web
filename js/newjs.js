15160f579625190312a4bfcfd424fa0f482e169a
nacosoyscatech-001 <nacosoyscatech@gmail.com>
2025-12-07 13:32:04 +0000

    re adding nacos web site

---
+++ js/newjs.js
@@ -0,0 +1,55 @@
+function gotohome1(){
+  if(window.innerWidth < 800){
+    closeham();
+    window.location.href="index.html#home";
+
+  }
+  else{
+    window.location.href="index.html#home";
+    
+  }
+}
+
+function gotocourse1(){
+  if(window.innerWidth < 800){
+    closeham()
+  window.location.href="index.html#course";
+    
+  }
+  else{
+  window.location.href="index.html#course";
+      
+
+    }
+}
+function gotolectures1(){
+    if(window.innerWidth < 800){
+      closeham()
+  window.location.href="index.html#lecturer";
+
+    }
+    else{
+      window.location.href="index.html#lecturer";
+
+    }
+}
+function gotogpcal1(){
+  if(window.innerWidth < 800){
+    closeham()
+  window.location.href="index.html#gpcal";
+  
+
+  }
+  window.location.href="index.html#gpcal";
+  
+}
+function gotoabout1(){
+    if(window.innerWidth < 800){
+      closeham();
+  window.location.href="index.html#about";
+    }
+    else{
+  window.location.href="index.html#about";
+
+    }
+}
